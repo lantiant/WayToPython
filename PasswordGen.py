4ca22536ed112dd42cd9e95e40c1899405283de7
lantiant <270185397@qq.com>
2018-02-27 19:21:52 +0800

    first version
    To generate  random strings of the length you need,default length is 8,max length is 40.

---
+++ PasswordGen.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+#coding: utf-8
+import string
+import random
+import sys
+
+'''用于生成具有一定强度的密码，建议最小长度为8位,最大长度不超过40位。'''
+
+p1 = list(string.ascii_lowercase)
+p2 = list(string.digits)
+p3 = list(string.punctuation)
+p4 = list(string.ascii_uppercase)
+random.shuffle(p1)
+random.shuffle(p2)
+random.shuffle(p3)
+random.shuffle(p4)
+
+
+def getnewlist(length=8):
+    length = int(sys.argv[1])
+    oldlist = [p1,p2,p3,p4]
+    lenlist = len(oldlist)
+    if length == 0 or length <= lenlist:
+        return oldlist[0:length]
+    else:
+        diff = length - lenlist
+        if diff <= lenlist:
+            return oldlist + oldlist[0:diff]
+        else:
+            multi = length // lenlist
+            rem = length % lenlist
+            newlist = oldlist * multi
+            return newlist + oldlist[0:rem]
+
+pwlist = []
+for i in getnewlist():
+    pw = i.pop()
+    pwlist.append(pw)
+
+random.shuffle(pwlist)
+
+print(''.join(pwlist))
+    
+
+
+
+
+
+    
+    

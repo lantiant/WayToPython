66ad1c7a05903d65e13352d8d69b4e281495863e
lantiant <270185397@qq.com>
2018-03-05 10:37:44 +0800

    add comment

--- PasswordGen.py
+++ PasswordGen.py
@@ -9,6 +9,7 @@ import sys
 p1 = list(string.ascii_lowercase)
 p2 = list(string.digits)
 p3 = list(string.punctuation)
+#如果确定某些特殊符号不能用作密码，可以将p4明确指定为由可以使用的特殊符号构成的列表。
 p4 = list(string.ascii_uppercase)
 random.shuffle(p1)
 random.shuffle(p2)
